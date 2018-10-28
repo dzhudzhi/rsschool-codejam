9c932179a853c2daaa40fe317806b44e24f65ba0
dzhudzhi <dzhudzhiev@gmail.com>
2018-10-28 20:34:12 +0300

    fix: function make now work with passed function

--- make.js
+++ make.js
@@ -1,14 +1,11 @@
 const make = (...args) => {
     const arr = [];
     const func = (...args) => {
-        if (typeof(args[0]) === 'function') return arr.slice(1).reduce((a, b, i) => arr[i] = a + b, arr[0]);
+        if (typeof(args[0]) === 'function') return arr.slice(1).reduce((a, b, i) => arr[i] = args[0](a ,b), arr[0]);
         arr.push(...args);
         return func;
     }
     return func(...args);
 }
 
-const sum = (a, b) => a + b;
-
-// console.log(make());
-console.log(make(14)(1, 2, 3)(sum));
+module.exports = make;
