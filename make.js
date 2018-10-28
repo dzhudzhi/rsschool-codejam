f7d655e38cbf60b7b2e4540e7b75672f346be6e3
dzhudzhi <dzhudzhiev@gmail.com>
2018-10-28 20:52:55 +0300

    refactor: refactor code in airbnb style

--- make.js
+++ make.js
@@ -1,11 +1,16 @@
 const make = (...args) => {
-    const arr = [];
-    const func = (...args) => {
-        if (typeof(args[0]) === 'function') return arr.slice(1).reduce((a, b, i) => arr[i] = args[0](a ,b), arr[0]);
-        arr.push(...args);
-        return func;
+  const arr = [];
+  const func = (...args) => {
+    if (typeof (args[0]) === 'function') {
+      return arr.slice(1).reduce((a, b, i) => {
+        arr[i] = args[0](a, b);
+        return arr[i];
+      }, arr[0]);
     }
-    return func(...args);
-}
+    arr.push(...args);
+    return func;
+  };
+  return func(...args);
+};
 
 module.exports = make;
--- recursion.js
+++ recursion.js
@@ -1,19 +1,19 @@
 const recursion = (tree) => {
-    const res = [];
-    const oneLevelNodes = (...args) => {
-        const thisLevel = [];
-        const nextLevel = [];
-        args.forEach(node => {
-            thisLevel.push(node.value);
-            if (node.left) nextLevel.push(node.left);
-            if (node.right) nextLevel.push(node.right);
-        });
-        res.push(thisLevel);
-        if (nextLevel.length !== 0) oneLevelNodes(...nextLevel); 
-    }
+  const res = [];
+  const oneLevelNodes = (...args) => {
+    const thisLevel = [];
+    const nextLevel = [];
+    args.forEach((node) => {
+      thisLevel.push(node.value);
+      if (node.left) nextLevel.push(node.left);
+      if (node.right) nextLevel.push(node.right);
+    });
+    res.push(thisLevel);
+    if (nextLevel.length !== 0) oneLevelNodes(...nextLevel); 
+  };
 
-    oneLevelNodes(tree);
-    return res;
-}
+  oneLevelNodes(tree);
+  return res;
+};
 
 module.exports = recursion;
